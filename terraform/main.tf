660468a7ab0fbf67b37433451e559840444b7d93
Pepires58 <pedrohenriquepires13@gmail.com>
2026-01-18 11:28:02 -0300

    updating version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.5.1"
+  version = "~> 5.0"
 
   name = "sideprojects-vpc"
   cidr = var.vpc_cidr
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,17 +4,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 6.0.0"
     }
   }
 
   backend "s3" {
-    # Backend configuration will be provided via backend config file or command line
-    # bucket         = "sideprojects-terraform-state"
-    # key            = "vpc/terraform.tfstate"
-    # region         = "us-east-1"
-    # dynamodb_table = "terraform-state-lock"
-    # encrypt        = true
+    bucket         = "pepires58-sideprojects-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "sa-east-1"
   }
 }
 
